6205b52194852f58e99e51f842b2475531529bdf
MAICON APARECIDO DE ALMEIDA <maiconalmeida@outlook.pt>
2020-06-28 20:25:20 -0300

    Add files via upload
    Encapsulamento - interface - POO

---
+++ src/br/com/maicon/aulaencapsulamento/AulaEncapsulamento.java
@@ -0,0 +1,16 @@
+package br.com.maicon.aulaencapsulamento;
+
+public class AulaEncapsulamento {
+
+    public static void main(String[] args) {
+        RemoteControl c = new RemoteControl();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+        c.desligar();
+
+    }
+
+}
